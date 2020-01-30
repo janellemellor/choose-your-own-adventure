e7f76ebc363927be7cdba87cc99ab1ee7569acaf
janelle mellor <janellemellor@gmail.com>
2020-01-30 11:34:35 -0800

    create user function

--- app.js
+++ app.js
@@ -7,16 +7,42 @@
 
 //set up main index.html page with form and ids needed
 //get data (name, character selection) from user input in home page
-const formData = document.getElementById('form');
+
+//create new variable userFormData that will go and get the element form from the index.html.
+const userFormData = document.getElementById('form');
 
 //create a function that takes a single instance of form data and pulls out user information to create a user object. 
 
-
-
-    //set initial state for carbon footprint tracking
-    //set initial state for number of trees earned
-    //tracks which quizzes the user has completed
-
+//declare a new function createUserObject that will take in some form data
+function createUserObject(someFormData) {
+    //create a user object/variable
+    const user = {
+        //input some key value pairs with the user information that we need at the start of the quiz:
+        //user name from the input
+        name: someFormData.get('user-name-input'),
+        //user character choice from the radio button
+        character: someFormData.get('character'),
+        //set initial state for carbon footprint tracking
+        cf: 0,
+        //set initial state for number of trees earned
+        trees: 0,
+        //tracks which quizzes the user has completed
+        completedQuizzes: {},
+    };
+} 
+//come back and test creatUserObject
+   
+    
 //create an event handler with submit to create the user object. 
+//add an event listener to the userFormData that will activate when the user submits the form. 
+userFormData.addEventListener('submit', (event) => {
+    //use to prevent the page from auto-refreshing
+    event.preventDefault();
+
+    
+
+)}
+
+
 //save the user object to local storage
 //redirect to the next quiz question    
