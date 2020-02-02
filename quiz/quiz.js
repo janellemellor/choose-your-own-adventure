7466324c5c768f6966ad9b7209d06615bf47cdd2
janelle mellor <janellemellor@gmail.com>
2020-02-01 17:53:36 -0800

    update quiz.js

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -15,20 +15,33 @@
         //call the question completed in state, and redirect to list/map page
 
       
-     
+     //create a label for each choice with span and input inside --
 
-        // Read the query parameter using UrlSearchParams built-in browser class
-        // Use your findById function to find the metadata item with that id
-        // redirect back to list page if id not found
+      
+       
         // Update the dom:
         // Static one-time things like title, image, etc.
         // Loop through choices and create form input controls (like radio buttons) for each choice
 
 //import metadata:
-import quizzes from '../data/quizData.js';    
+import quizzes from '../data/quizData.js';
+import { findById } from '../quiz/quiz-utils.js';   
 
 //get form from DOM
 const quizForm = document.getElementById('quiz-form');
 
 
-  //create a label for each choice with span and input inside --
+//create searchParams which will go into the URL and find the search parameters. URLSearchParams is built-in.
+const searchParams = new URLSearchParams(window.location.search);
+
+//get the quiz id from the URL search params
+const quizId = searchParams.get('id');
+
+ // Use findById function to find the quiz from the quiz metadata by searching by id
+const findQuizById = findById(quizzes, quizId);
+
+// if there is no quiz id found, redirect to the quiz map page
+if (!findQuizById) {
+    window.location = '../quiz-map/';
+} 
+  
