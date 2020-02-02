6e9b85acba2f094fa52c5b76a94c0b61a184ad31
janelle mellor <janellemellor@gmail.com>
2020-02-01 22:26:11 -0800

    calculate scores

--- results/results.js
+++ results/results.js
@@ -17,25 +17,41 @@ import { getUserStatus } from '../common/utils.js';
 
 //get DOM
 const finalResults = document.getElementById('final-results');
-const resultsSection = document.getElementById('results-section');
+//may not need this- not hiding this section?
+// const resultsSection = document.getElementById('results-section');
 
 
 // get the user choices from local storage
 const user = getUserStatus();
 
 
-
-
-// Testing: write function(s) to test conditions of user object
+// Write function(s) to test conditions of user object
 //count the number of answers that correspond to each category and get a winner
-//based on the answers, display corresponding results based on cf points.
 
 function calculateCarbonFootprint(cf) {
     if (cf <= 100) {
-        return 'low';
+        return 'Keep up the good work! You have a low carbon footprint';
     } else if (cf <= 200 && cf > 100) {
-        return 'mid';    
+        return 'Great job! You are doing a lot to keep Portland Green!';    
     } else {
-        return 'high';
+        return 'Good effort!';
     }
 }
+
+function calculateNumberOfTreesEarned(trees) {
+    if (trees <= 3) {
+        return 'As a reward, you get to plant three trees!';
+    } else if (trees <= 7 && trees > 3) {
+        return 'Thanks for volunteering to plant five trees!';
+    } else {
+        return 'Can you help us plant all these trees?';
+    }
+}
+
+//based on the answers, display corresponding results based on cf points.
+const cfScore = calculateCarbonFootprint(user.cf);
+const numberOfTreesEarned = calculateNumberOfTreesEarned(user.trees);
+
+
+
+finalResults.textContent = `Thank you for playing! ${cfScore} + ${numberOfTreesEarned}`;
